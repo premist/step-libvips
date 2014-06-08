a2571d8f9f15d54eb9f43e9820f87ec542cbc449
Minku Lee <premist@me.com>
2014-06-08 20:55:09 +0900

    Initial commit

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+cd $HOME
+
+info 'Installing libtiff4-dev'
+sudo add-apt-repository ppa:lyrasis/precise-backports -y
+sudo apt-get update -y
+sudo apt-get install libtiff4-dev -y
+success 'Installed libtiff4-dev'
+
+info 'Installing build tools'
+sudo apt-get install automake build-essential git gobject-introspection gtk-doc-tools libfftw3-dev libglib2.0-dev libjpeg-turbo8-dev libpng12-dev libwebp-dev libtiff5-dev libexif-dev libxml2-dev swig -y
+success 'Installed build tools'
+
+info 'Cloning libvips'
+git clone https://github.com/jcupitt/libvips.git
+cd libvips
+git checkout 7.38
+success 'Cloned libvips'
+
+info 'Installing libvips'
+./bootstrap.sh
+./configure --enable-debug=no --enable-cxx=no --without-python --without-orc
+make
+sudo make install
+sudo ldconfig
+success 'Installed libvips'
